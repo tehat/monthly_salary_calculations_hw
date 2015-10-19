480056c863780246d516a7fc1daff9f13988ef7b
tehat <thomasehartle@gmail.com>
2015-10-19 08:30:35 -0500

    second commit

--- app.js
+++ app.js
@@ -5,37 +5,41 @@ $(document).ready(function(){
 
 		var values = {};
 		
-		console.log($("#employeeinformation").serializeArray());
+		$("#employeeinformation").serializeArray();
 		//The serializeArray() method creates an array of objects (name and value) by serializing form values.
-		$.each($("employeeinformation").serializeArray(), function(i, field){
+		$.each($("#employeeinformation").serializeArray(), function(i, field){
 			values[field.name] = field.value;
-			
 			//$.post(field.value);
 		})
-		
-
 		$("#employeeinformation").find("input[type=text]").val("");
-		console.log(values);
 		employeeArray.push(values);  //push employeeinformation objects into array
 		appendDom(values);
-		console.log(values);
-
 	});
 });
-var employeeArray = [employeeName, employeeNumber, employeeJobTitle, employeeYearlySalary];
-var saleriesPerMonth = 0;
-var salaries = 0;
 
 
+ var employeeArray = [];
 
+// var saleriesPerMonth = [];
+// var totalMonthlyPayroll = [];
+
+// for(var i = 0; i < employeeArray.length; i++) {
+// 	saleriesPerMonth = employeeArray.pop();
+// };
+// console.log(saleriesPerMonth);
+
+//   $("#employeeContainer").on('click', '.someButton', function() {
+// +    //console.log($(this).parent().find('p').text());
+// +    $(this).parent().remove();
+// +  });
 
 
 function appendDom(employee){
 	
-	console.log($("#employeeContainer").append("<div class='employee'></div>"));
-	var $el = $("employeeContainer").children().last();
+	($("#employeeContainer").append("<div class='employee'></div>"));
+	var $el = $("#employeeContainer").children().last();
 
-	$el.append("<p>" + employee.employeefirstlastname + "</p>");
+	$el.append("<p>" + employee.employeename + "</p>");
 	$el.append("<p>" + employee.employeenumber + "</p>");
 	$el.append("<p>" + employee.employeejobtitle + "</p>");
 	$el.append("<p>" + employee.employeeyearlysalary + "</p>");
